202df08dd9b25214d1b8c3ac83133d7af032916b
Min Lu <70182873+minlu21@users.noreply.github.com>
2022-03-27 02:57:53 -0400

    edited

--- src/main/java/com/example/theatre/controller/user/SignupController.java
+++ src/main/java/com/example/theatre/controller/user/SignupController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-
 /**
  *
  * @author Shicheng Fang
--- src/main/java/com/example/theatre/entity/User.java
+++ src/main/java/com/example/theatre/entity/User.java
@@ -3,6 +3,7 @@ package com.example.theatre.entity;
 import javax.persistence.*;
 
 @Entity
+@Table(name = "Users")
 public class User {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
