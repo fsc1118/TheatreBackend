e6ea12a1ecaa197a13cb0c0818a73618aedc787d
Min Lu <70182873+minlu21@users.noreply.github.com>
2022-04-11 17:17:49 -0400

    edited application.properties

--- src/main/java/com/example/theatre/entity/User.java
+++ src/main/java/com/example/theatre/entity/User.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 public class User {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long U_id;
 
     @Column(nullable = false)
