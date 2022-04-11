350051d0b64e406ea0d130666efcc7938adc4d99
Min Lu <70182873+minlu21@users.noreply.github.com>
2022-04-11 17:00:24 -0400

    added seat service

---
+++ src/main/java/com/example/theatre/service/SeatService.java
@@ -0,0 +1,31 @@
+package com.example.theatre.service;
+
+import com.example.theatre.entity.Seat;
+import com.example.theatre.composite_keys.SeatPK;
+import com.example.theatre.repository.SeatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SeatService {
+    @Autowired
+    private final SeatRepository seatRepository;
+
+    public SeatService(SeatRepository seatRepository) {
+        this.seatRepository = seatRepository;
+    }
+
+    public void insertNewSeat(Integer seat_num, Integer room_id) {
+        this.seatRepository.save(new Seat(new SeatPK(seat_num, room_id)));
+    }
+
+    public List<Integer> getAllSeatsFromRoom(Integer room_id) {
+        return seatRepository.getAllSeats(room_id);
+    }
+
+    public List<Seat> findAll() {
+        return seatRepository.findAll();
+    }
+}
