98ddbf3622211caed0de988b91cdc45f268c5c15
Min Lu <70182873+minlu21@users.noreply.github.com>
2022-04-11 16:48:22 -0400

    add ticket entity

---
+++ src/main/java/com/example/theatre/entity/Ticket.java
@@ -0,0 +1,70 @@
+package com.example.theatre.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@Entity
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ticket_id")
+    private Long ticket_id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id",
+            foreignKey = @ForeignKey(name = "FK__ticket_user"), insertable = false, updatable = false)
+    @JsonBackReference
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id",
+            foreignKey = @ForeignKey(name = "FK__ticket_movie"), insertable = false, updatable = false)
+    @JsonBackReference
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id",
+            foreignKey = @ForeignKey(name = "FK__ticket_room"), insertable = false, updatable = false)
+    @JsonBackReference
+    private Room room;
+
+    @ManyToOne
+    @JoinColumns(value = {
+            @JoinColumn(name = "room_id", insertable = false, updatable = false),
+            @JoinColumn(name = "seat_num", insertable = false, updatable = false),
+    },
+            foreignKey = @ForeignKey(name = "FK__ticket_seat")
+    )
+    @JsonBackReference
+    private Seat seat;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "purchase_datetime")
+    private Date purchase_datetime;
+
+    @Column(name = "is_used")
+    private Boolean is_used;
+
+    @Column(name = "show_datetime")
+    private Date show_datetime;
+
+    public Ticket(Double price, Date purchase_datetime, Boolean is_used, Date show_datetime) {
+        this.price = price;
+        this.purchase_datetime = purchase_datetime;
+        this.is_used = is_used;
+        this.show_datetime = show_datetime;
+    }
+}
