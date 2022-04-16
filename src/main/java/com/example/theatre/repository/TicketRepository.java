5e4e658725e5746f5ecda6a1e57d74357d9760ae
Min Lu <70182873+minlu21@users.noreply.github.com>
2022-04-16 00:17:51 -0400

    added ticket repository and service

---
+++ src/main/java/com/example/theatre/repository/TicketRepository.java
@@ -0,0 +1,15 @@
+package com.example.theatre.repository;
+
+import com.example.theatre.entity.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+    @Procedure(procedureName = "sp_buyTicket")
+    int addTicket(String movieName, Integer roomId, Date showDatetime, Integer seatNum, Long userId); // success = 0, not success = 1
+}
