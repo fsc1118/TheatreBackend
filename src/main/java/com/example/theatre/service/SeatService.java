5af95e7923a2b0fd36e1cbe107ce22a84a9032bf
Min Lu <70182873+minlu21@users.noreply.github.com>
2022-04-16 01:25:03 -0400

    added method to get available seats from a movieshowing

--- src/main/java/com/example/theatre/service/SeatService.java
+++ src/main/java/com/example/theatre/service/SeatService.java
@@ -5,7 +5,11 @@ import com.example.theatre.composite_keys.SeatPK;
 import com.example.theatre.repository.SeatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 @Service
@@ -17,12 +21,23 @@ public class SeatService {
         this.seatRepository = seatRepository;
     }
 
-    public void insertNewSeat(Integer seat_num, Integer room_id) {
-        this.seatRepository.save(new Seat(new SeatPK(seat_num, room_id)));
+    private final SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    private Timestamp parseTimestamp(String timestamp) {
+        try {
+            return new Timestamp(DATE_TIME_FORMAT.parse(timestamp).getTime());
+        } catch (ParseException e) {
+            throw new IllegalArgumentException(e);
+        }
     }
 
     public List<Integer> getAllSeatsFromRoom(Integer room_id) {
         return seatRepository.getAllSeats(room_id);
+    }
+
+    @Transactional
+    public List<Integer> getAllAvailableSeatsOfMovieShowing(Long movieId, Integer roomId, String showDatetime) {
+        return seatRepository.getAllAvailableSeats(movieId, roomId, parseTimestamp(showDatetime));
     }
 
     public List<Seat> findAll() {
